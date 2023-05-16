f7235c30b4a98562e0acf610c41f7d07ebfb3a62
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-16 14:22:28 -0300

    Feat: Home page app
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <section>
-      <h1>Hello World</h1>
+    <section className="flex h-screen w-full flex-1 flex-col items-center justify-center gap-4 bg-zinc-950">
+      <h1 className="text-6xl font-bold text-zinc-50">Your Time Capsule</h1>
+      <p className="text-base text-zinc-400">
+        Collect memorable moments from your journey and share (if you like) with
+        the world!
+      </p>
     </section>
   );
 }
