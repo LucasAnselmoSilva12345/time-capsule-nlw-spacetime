2ed8687762b1f9f203fb03020c2cb9e0837dcaf2
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-16 20:07:41 -0300

    Feat: Fonts of use in App
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <section className="flex h-screen w-full flex-1 flex-col items-center justify-center gap-4 bg-zinc-950">
       <h1 className="text-6xl font-bold text-zinc-50">Your Time Capsule</h1>
+      <h1 className=" font-second text-6xl font-bold text-zinc-50">
+        Your Time Capsule
+      </h1>
       <p className="text-base text-zinc-400">
         Collect memorable moments from your journey and share (if you like) with
         the world!
