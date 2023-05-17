190fa8c628479325086c9daf6b9d0a87c8eae724
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-16 21:38:15 -0300

    Feat: Components of homepage
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/app/components/LeftSection.tsx
@@ -0,0 +1,17 @@
+import Blur from "@/components/Blur";
+import Copyright from "@/components/Copyright";
+import HeroSection from "@/components/HeroSection";
+import SignIn from "@/components/SignIn";
+import Stripes from "@/components/Stripes";
+
+export default function LeftSection() {
+  return (
+    <section className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+      <Blur />
+      <Stripes />
+      <SignIn />
+      <HeroSection />
+      <Copyright />
+    </section>
+  );
+}
