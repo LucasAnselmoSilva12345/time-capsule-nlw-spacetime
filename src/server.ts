c0e444ccd2536ed8926f90de5bbdffb0cc7cb746
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-18 19:22:37 -0300

    Feat: Require authenticate for access routes Feat: Host access on server
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 
 app.register(cors, {
   origin: true,
 });
+
+app.register(jwt, {
+  secret: 'spacetime',
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('Server is running on port 3333');
